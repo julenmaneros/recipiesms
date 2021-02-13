6cb14613dfd6f5f445206f105ce22cbd154c4195
Julen <julen@apolo.local>
2021-02-14 00:50:58 +0100

    Remove HTTPS middleware

--- Startup.cs
+++ Startup.cs
@@ -36,8 +36,6 @@ namespace recipiesms
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
