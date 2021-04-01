53f3edaa1bb9315366bbcd79d680e79e1ea36dab
Julen <julen@apolo.local>
2021-04-01 20:26:13 +0200

    Add validation to recipie creation

--- Controllers/RecipiesController.cs
+++ Controllers/RecipiesController.cs
@@ -31,6 +31,16 @@ namespace recipiesms.Controllers
         [HttpPost]
         public IActionResult CreateRecipie([FromBody] RecipieForCreationDto recipie)
         {
+            if (recipie.Title.Contains("kaka"))
+            {
+                ModelState.AddModelError("Title", "kaka is not allowed in the title.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var recipieId = RecipiesDataStore.Current.Recipies.Count + 1;
 
             var finalRecipie = new RecipieDto()
--- Models/RecipieForCreationDto.cs
+++ Models/RecipieForCreationDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace recipiesms.Models
 {
     public class RecipieForCreationDto
     {
+        [Required(ErrorMessage="Title is required for the recipie")]
+        [MaxLength(50)]
         public string Title { get; set; }
     }
 }
